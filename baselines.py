11fa9394f2fe8c9e1f7d5bbb068c905d3237c9ff
Rasmus Krusaa <rasmuskrusaa@gmail.com>
2019-10-14 12:08:12 +0200

    baselines eval

--- baselines.py
+++ baselines.py
@@ -1,19 +1,22 @@
 import random
 import numpy as np
+import pandas as pd
 
-def recommend_random(k : int, max_item_idx : int):
+import evaluation
+
+
+def recommend_random(max_item_idx : int):
     """
-    :param k: number of items to recommend
     :param max_item_idx: largest index of items
-    :returns: list of k recommended items indices
+    :return: list of recommended items indices
     """
-    return random.sample(range(1, max_item_idx), k)
+    # TODO: consider if we should just  return k elements
+    return random.sample(range(1, max_item_idx + 1), max_item_idx)
 
-def most_popular(k: int, R : np.ndarray):
+def most_popular(R : np.ndarray):
     """
-    :param k: number of items to recommend
     :param R: user-item interaction matrix
-    :return: list of k recommended items indices
+    :return: list of recommended items indices
     """
     summed_R_on_items = np.sum(R, axis=0) # summing items' ratings
     n_ratings_on_items = np.count_nonzero(R, axis=0) # counting number of items' ratings
@@ -22,6 +25,27 @@ def most_popular(k: int, R : np.ndarray):
 
     sorted_item_idx = np.argsort(item_avgs) # sorting items' avg ratings and gives a list with indices
 
-    return sorted_item_idx[-k:] # return k last elements (k items with highest avg rating)
+    # TODO: consider if we should just  return k elements
+    return np.flip(sorted_item_idx) # return sorted list reverse
 
+if __name__ == '__main__':
+    mp_precision = 0
+    random_precision = 0
+
+    for split in range(1, 6):
+        data = pd.read_csv(f'data/test{split}.csv')
+
+        # most popular
+        mp_relevance = evaluation.compute_relevance_for_most_popular(data, 10)
+        mp_precision += evaluation.precision_at_k(mp_relevance, 10)
+
+        # random
+        random_relevance = evaluation.compute_relevance_for_random(data, 10)
+        random_precision += evaluation.precision_at_k(random_relevance, 10)
+
+    mp_precision /= 5
+    random_precision /= 5
+
+    print(f'Most popular\'s precision@10: {mp_precision}')
+    print(f'Random\'s precision@10: {random_precision}')
 
--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 
+import baselines
+import utils
+
 
 def mse(predictions: np.ndarray, ratings: np.ndarray):
     xs, ys = ratings.nonzero()
@@ -38,6 +41,83 @@ def ndcg_at_k(relevance, k):
 
     return res / len(relevance)
 
+def compute_relevance_for_most_popular(actuals : pd.DataFrame, k : int):
+    """
+    Computes the binary relevance vector for each user,
+    meaning 0 if bad recommendation, 1 if good recommendation.
+
+    :param actuals: actuals: n_ratings x [user, item, rating, timestamp] matrix with actual ratings
+    :param k: number of items to recommend
+    :return: a list of lists, where each list is a binary relevance vector for a user
+    """
+    np_actuals = utils.to_ndarray(actuals)
+    mp_items = baselines.most_popular(np_actuals)
+
+    # list of users
+    users = actuals['user'].unique()
+
+    # result
+    res = []
+
+    # iterating over users to make binary relevance vectors
+    for user in users:
+        data = actuals[actuals['user'] == user] # taking data for user.
+        items_rated = list(data['item']) # taking items he has rated, as only those can be evaluated.
+
+        # TODO: I think we should just take k first elements recommended list and then call "not-rated" items false positives
+        # taking k items of most popular which the user has rated.
+        k_mp_items = [i for i in mp_items if i in items_rated][:k]
+
+        if len(k_mp_items) > 0:
+            relevance = compute_single_relevance_for_index_predictions(data, k_mp_items)
+            res.append(relevance)
+
+    return res
+
+def compute_relevance_for_random(actuals : pd.DataFrame, k : int):
+    """
+        Computes the binary relevance vector for each user,
+        meaning 0 if bad recommendation, 1 if good recommendation.
+
+        :param actuals: actuals: n_ratings x [user, item, rating, timestamp] matrix with actual ratings
+        :param k: number of items to recommend
+        :return: a list of lists, where each list is a binary relevance vector for a user
+        """
+    # list of users
+    users = actuals['user'].unique()
+    max_item_idx = actuals['item'].max()
+
+    # result
+    res = []
+
+    # iterating over users to make binary relevance vectors
+    for user in users:
+        random_rec = baselines.recommend_random(max_item_idx)
+
+        data = actuals[actuals['user'] == user]  # taking data for user.
+        items_rated = list(data['item'])  # taking items he has rated, as only those can be evaluated.
+
+        # TODO: I think we should just take k first elements recommended list and then call "not-rated" items false positives
+        # taking k items of most popular which the user has rated.
+        k_random_items = [i for i in random_rec if i in items_rated][:k]
+
+        if len(k_random_items) > 0:
+            relevance = compute_single_relevance_for_index_predictions(data, k_random_items)
+            res.append(relevance)
+
+    return res
+
+
+def compute_single_relevance_for_index_predictions(actuals, index_predictions):
+    """
+    :param actuals: n_ratings x [user, item, rating, timestamp] matrix for a user
+    :param index_predictions: list of item indices
+    :return: binary relevance vector, 1 if good recommendation, 0 if bad
+    """
+    relevance = [1 if (actuals[actuals['item'] == i]['rating'] > 3).iat[0]
+                 else 0
+                 for i in index_predictions]
+    return relevance
 
 def compute_relevance(actuals : pd.DataFrame, predictions : pd.DataFrame):
     """
