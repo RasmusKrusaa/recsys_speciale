b34de1ceb8754b74cc5b5fafbe01a1a0cfabc46a
Rasmus Krusaa <rasmuskrusaa@gmail.com>
2020-04-01 12:49:37 +0200

    Questionnaire added to model

--- EATNN/EATNN.py
+++ EATNN/EATNN.py
@@ -67,50 +67,68 @@ class EATNN:
         self.weight_r = 0.1
         # weighting for entries in X
         self.weight_x = 0.1
+        # weighting for entries in Q
+        self.weight_q = 0.1
         # parameter to adjust weight of social interactions
         self.mu = 0.1
         # regularization terms
-        self.lambda_bilinear = [1e-1, 1e-2, 1e-3]
+        self.lambda_bilinear = [1e-3, 1e-1, 1e-2, 1e-1]
 
     def _create_variables(self):
         """
         Creates/instantiates variables for model such that we can look them up when learning and making predictions
         """
-        # Weights to map user embeddings into embeddings representing shared knowledge in item and social domains (u^c in paper)
+        # Weights to map uid into embeddings representing shared knowledge in item and social domains (u^c in paper)
         self.uw_c = tf.Variable(tf.random.truncated_normal(shape=[self.n_users, self.embedding_size],
                                                                 mean=0.0, stddev=0.01, dtype = tf.float32, name='uc'))
-        # Weights to map user embeddings into embeddings representing preferences to items (u^i in paper)
+        # Weights to map uid into embeddings representing preferences to items (u^i in paper)
         self.uw_i = tf.Variable(tf.random.truncated_normal(shape=[self.n_users, self.embedding_size],
                                                               mean=0.0, stddev=0.01, dtype=tf.float32, name='ui'))
-        # Weights to map user embeddings into embeddings preferences to items (u^s in paper)
+        # Weights to map uid into embeddings preferences to items (u^s in paper)
         self.uw_s = tf.Variable(tf.random.truncated_normal(shape=[self.n_users, self.embedding_size],
                                                                 mean=0.0, stddev=0.01, dtype=tf.float32, name='us'))
-        # Embeddings for items (q in paper)
+        # Weights to map uid into embeddings for questionnaires
+        self.uw_q = tf.Variable(tf.random.truncated_normal(shape=[self.n_users, self.embedding_size],
+                                                                mean=0.0, stddev=0.01, dtype=tf.float32, name='uq'))
+        # TODO: figure out why they +1 in sizes
+        # Embeddings for items (M x D)
         self.Q = tf.Variable(tf.random.truncated_normal(shape=[self.n_items + 1, self.embedding_size],
                                                         mean=0.0, stddev=0.01, dtype=tf.float32, name='Q'))
-        # Embeddings for social interactions (g in paper)
+        # Embeddings for social interactions (N x D)
         self.G = tf.Variable(tf.random.truncated_normal(shape=[self.n_users + 1, self.embedding_size],
                                                         mean=0.0, stddev=0.01, dtype=tf.float32, name='G'))
+        # Embeddings for questionnaires (M x D)
+        self.V = tf.Variable(tf.random.truncated_normal(shape=[self.n_items + 1, self.embedding_size],
+                                                        mean=0.0, stddev=0.01, dtype=tf.float32, name='P'))
+
 
         # Weights used in item domain prediction layer
         self.H_i = tf.Variable(tf.constant(0.01, shape=[self.embedding_size, 1]), name='hi')
-
         # Weights used in social domain prediction layer
         self.H_s = tf.Variable(tf.constant(0.01, shape=[self.embedding_size, 1]), name='hf')
+        # Weights used in questionnaire domain prediction layer
+        self.H_q = tf.Variable(tf.constant(0.01, shape=[self.embedding_size, 1]), name='hq')
 
         # Item domain attention network parameters
-        self.W_alpha = tf.Variable(
+        self.W_item = tf.Variable(
             tf.random.truncated_normal(shape=[self.embedding_size, self.attention_size], mean=0.0, stddev=tf.sqrt(
-                tf.divide(2.0, self.attention_size + self.embedding_size))), dtype=tf.float32, name='Walpha')
-        self.B_alpha = tf.Variable(tf.constant(0.00, shape=[self.attention_size]), name='Balpha') # 1 x k
-        self.H_alpha = tf.Variable(tf.constant(0.01, shape=[self.attention_size, 1], name='Halpha')) # k x 1
+                tf.divide(2.0, self.attention_size + self.embedding_size))), dtype=tf.float32, name='Witem')
+        self.B_item = tf.Variable(tf.constant(0.00, shape=[self.attention_size]), name='Bitem') # 1 x k
+        self.H_item = tf.Variable(tf.constant(0.01, shape=[self.attention_size, 1], name='Hitem')) # k x 1
 
         # Social domain attention network parameters
-        self.W_beta = tf.Variable(
+        self.W_social = tf.Variable(
             tf.random.truncated_normal(shape=[self.embedding_size, self.attention_size], mean=0.0, stddev=tf.sqrt(
-                tf.divide(2.0, self.attention_size + self.embedding_size))), dtype=tf.float32, name='Wbeta')
-        self.B_beta = tf.Variable(tf.constant(0.00, shape=[self.attention_size]), name='Bbeta')  # 1 x k
-        self.H_beta = tf.Variable(tf.constant(0.01, shape=[self.attention_size, 1], name='Hbeta'))  # k x 1
+                tf.divide(2.0, self.attention_size + self.embedding_size))), dtype=tf.float32, name='Wsocial')
+        self.B_social = tf.Variable(tf.constant(0.00, shape=[self.attention_size]), name='Bsocial')  # 1 x k
+        self.H_social = tf.Variable(tf.constant(0.01, shape=[self.attention_size, 1], name='Hsocial'))  # k x 1
+
+        # Questionnaire domain attention network parameters
+        self.W_question = tf.Variable(
+            tf.random.truncated_normal(shape=[self.embedding_size, self.attention_size], mean=0.0, stddev=tf.sqrt(
+                tf.divide(2.0, self.attention_size + self.embedding_size))), dtype=tf.float32, name='Wquestion')
+        self.B_question = tf.Variable(tf.constant(0.00, shape=[self.attention_size]), name='Bquestion')  # 1 x k
+        self.H_question = tf.Variable(tf.constant(0.01, shape=[self.attention_size, 1], name='Hquestion'))  # k x 1
 
     def _create_placeholders(self):
         self.input_u = tf.placeholder(tf.int32, [None, 1], name="input_uid")
@@ -118,6 +136,7 @@ class EATNN:
 
         self.input_ur = tf.placeholder(tf.int32, [None, self.max_item_pu], name="input_ur")
         self.input_uf = tf.placeholder(tf.int32, [None, self.max_friend_pu], name="input_ur")
+        self.input_uq = tf.placeholder(tf.int32, [None, self.n_items], name='input_uq')
 
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
@@ -125,25 +144,53 @@ class EATNN:
         """
         Item attentive transfer (eq. 5)
         """
-        item_w = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_i, self.W_alpha) + self.B_alpha), self.H_alpha))
-        common_w = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_c, self.W_alpha) + self.B_alpha), self.H_alpha))
-        A_item = tf.divide(item_w, item_w + common_w)
-        A_common = 1.0 - A_item
-        P_iu = A_item * self.u_i + A_common * self.u_c
+        # eq. 3 for computing attentions
+        item_attention = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_i, self.W_item) + self.B_item), self.H_item))
+        common_attention = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_c, self.W_item) + self.B_item), self.H_item))
+        # eq. 4 for computing weights
+        item_weight = tf.divide(item_attention, item_attention + common_attention)
+        common_weight = 1.0 - item_weight
+        # eq. 5 for computing transferred user embedding
+        user_embedding = item_weight * self.u_i + common_weight * self.u_c
 
-        return P_iu, item_w
+        # returning user embedding, so that we can make predictions. Item weight such that we can analyse,
+        # when it's low or high
+        return user_embedding, item_weight
 
     def _social_attentive_transfer(self):
         """
         Social attentive transfer (eq. 5)
         """
-        social_w = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_s, self.W_beta) + self.B_beta), self.H_beta))
-        common_w = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_c, self.W_beta) + self.B_beta), self.H_beta))
-        A_social = tf.divide(social_w, social_w + common_w)
-        A_common = 1.0 - A_social
-        P_su = A_social * self.u_s + A_common * self.u_c
+        # eq. 3 for computing attentions
+        social_attention = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_s, self.W_social) + self.B_social), self.H_social))
+        common_attention = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_c, self.W_social) + self.B_social), self.H_social))
+        # eq. 4 for computing weights
+        social_weight = tf.divide(social_attention, social_attention + common_attention)
+        common_weight = 1.0 - social_weight
+        # eq. 5 for computing transferred user embedding
+        user_embedding = social_weight * self.u_s + common_weight * self.u_c
 
-        return P_su, social_w
+        # returning user embedding, so that we can make predictions. Social weight such that we can analyse,
+        # when it's low or high
+        return user_embedding, social_weight
+
+    def _questionnaire_attentive_transfer(self):
+        """
+        Transferring knowledge from questionnaires. Our contribution.
+        Works the same way, as the attentive networks in paper.
+        """
+        # eq. 3 for computing attentions
+        q_attention = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_q, self.W_question) + self.B_question), self.H_question))
+        common_attention = tf.exp(tf.matmul(tf.nn.relu(tf.matmul(self.u_c, self.W_question) + self.B_question), self.H_question))
+        # eq. 4 for computing weights
+        q_weight = tf.divide(q_attention, q_attention + common_attention)
+        common_weight = 1.0 - q_weight
+        # eq. 5 for computing transferred user embedding
+        user_embedding = q_weight * self.u_q + common_weight * self.u_c
+
+        # returning user embedding, so that we can make predictions. Social weight such that we can analyse,
+        # when it's low or high
+        return user_embedding, q_weight
 
     def _create_inference(self):
         """
@@ -156,13 +203,22 @@ class EATNN:
         self.u_i = tf.reshape(self.u_i, [-1, self.embedding_size])
         self.u_s = tf.nn.embedding_lookup(self.uw_s, self.input_u)
         self.u_s = tf.reshape(self.u_s, [-1, self.embedding_size])
+        # Our contribution (haven't added u^c2 because I figured, if we use the same u^c, we essentially use
+        # shared knowledge between all domains. Maybe that is favorable?
+        # TODO: We could test with only sharing between item domain and questionnaire domain.
+        self.u_q = tf.nn.embedding_lookup(self.uw_q, self.input_u)
+        self.u_q = tf.reshape(self.u_q, [-1, self.embedding_size])
 
         # Attentive transferred embeddings for users (p^I_u and p^S_u)
         self.P_iu, self.item_w = self._item_attentive_transfer()
         self.P_su, self.social_w = self._social_attentive_transfer()
+        # Our contribution
+        self.P_qu, self.question_w = self._questionnaire_attentive_transfer()
+
         # adding dropout on transferred embeddings to avoid overfitting
         self.P_iu = tf.nn.dropout(self.P_iu, self.dropout_keep_prob)
         self.P_su = tf.nn.dropout(self.P_su, self.dropout_keep_prob)
+        self.P_qu = tf.nn.dropout(self.P_qu, self.dropout_keep_prob)
 
         # Looking up item embeddings from data
         self.pos_item = tf.nn.embedding_lookup(self.Q, self.input_ur)
@@ -187,6 +243,18 @@ class EATNN:
         # Need to multiply with H_s as well
         self.pos_f = tf.einsum('abc,cd->abd', self.pos_friend, self.H_s)
         self.pos_f = tf.reshape(self.pos_f, [-1, max_social_pu])
+
+        # Questionnaire embeddings lookup
+        self.pos_questions = tf.nn.embedding_lookup(self.V, self.input_uq)
+        # Answered questions for this inference
+        self.pos_n_questions = tf.cast(tf.not_equal(self.input_uq, self.n_items), 'float32')
+        # Obtaining embeddings for questions used in this inference
+        self.pos_questions = tf.einsum('ab,abc->abc', self.pos_n_questions, self.pos_questions)
+        # Multiplying with question attentive transferred user embeddings
+        self.pos_q = tf.einsum('ac,abc->abc', self.P_qu, self.pos_questions)
+        # Need to multiply with H_q as well
+        self.pos_q = tf.einsum('abc,cd->abd', self.pos_questions, self.H_q)
+        self.pos_q = tf.reshape(self.pos_q, [-1, self.n_items])
 
     def _prediction(self):
         """
@@ -218,18 +286,30 @@ class EATNN:
                     tf.matmul(self.H_s, self.H_s, transpose_b=True), 0), 0)
         self.loss_social += tf.reduce_sum((1.0 - self.weight_x) * tf.square(self.pos_f) - 2.0 * self.pos_f)
 
-        # adding l2 regularization on social and item attentive user embeddings
-        self.l2_loss = tf.nn.l2_loss(self.P_iu + self.P_su)
-        # l2 regularization on item domain specific model parameters
-        self.l2_loss_item = tf.nn.l2_loss(self.W_alpha) + tf.nn.l2_loss(self.B_alpha) + tf.nn.l2_loss(self.H_alpha)
-        # l2 regularization on social domain specific model parameters
-        self.l2_loss_social = tf.nn.l2_loss(self.W_beta) + tf.nn.l2_loss(self.B_beta) + tf.nn.l2_loss(self.H_beta)
+        # TODO: consider this loss for questionnaire
+        self.loss_question = self.weight_q * tf.reduce_sum(
+            tf.reduce_sum(
+                tf.reduce_sum(
+                    tf.einsum('ab,ac->abc', self.V, self.V), 0) *
+                    tf.reduce_sum(tf.einsum('ab,ac->abc', self.P_qu, self.P_qu), 0) *
+                    tf.matmul(self.H_q, self.H_q, transpose_b=True), 0), 0)
+        self.loss_item += tf.reduce_sum((1.0 - self.weight_q) * tf.square(self.pos_q) - 2.0 * self.pos_q)
+
+        # adding l2 regularization on social, item and questionnaire attentive user embeddings
+        self.l2_loss = tf.nn.l2_loss(self.P_iu + self.P_su + self.P_qu)
+        # l2 regularization on item domain model parameters
+        self.l2_loss_item = tf.nn.l2_loss(self.W_item) + tf.nn.l2_loss(self.B_item) + tf.nn.l2_loss(self.H_item)
+        # l2 regularization on social domain model parameters
+        self.l2_loss_social = tf.nn.l2_loss(self.W_social) + tf.nn.l2_loss(self.B_social) + tf.nn.l2_loss(self.H_social)
+        # l2 regularization on questionnaire domain model parameters
+        self.l2_loss_question = tf.nn.l2_loss(self.W_question) + tf.nn.l2_loss(self.B_question) + tf.nn.l2_loss(self.H_question)
 
         # adding everything together
-        self.loss = self.loss_item + self.mu * self.loss_social \
+        self.loss = self.loss_item + self.mu * self.loss_social + self.mu * self.loss_question \
                     + self.lambda_bilinear[0] * self.l2_loss \
                     + self.lambda_bilinear[1] * self.l2_loss_item \
-                    + self.lambda_bilinear[2] * self.l2_loss_social
+                    + self.lambda_bilinear[2] * self.l2_loss_social \
+                    + self.lambda_bilinear[3] * self.l2_loss_question
 
     def build_graph(self):
         """
@@ -242,7 +322,7 @@ class EATNN:
         self.prediction = self._prediction()
 
 
-def train_step(u_batch, i_batch, s_batch):
+def train_step(u_batch, i_batch, s_batch, q_batch):
     """
     A single training step
     """
@@ -251,11 +331,12 @@ def train_step(u_batch, i_batch, s_batch):
         model.input_u: u_batch,
         model.input_ur: i_batch,
         model.input_uf: s_batch,
+        model.input_uq: q_batch,
         model.dropout_keep_prob: 0.3,
     }
-    _, loss, wi, ws = sess.run(
-        [train_op, model.loss, model.item_w, model.social_w], feed_dict)
-    return loss, wi, ws
+    _, loss, wi, ws, wq = sess.run(
+        [train_op, model.loss, model.item_w, model.social_w, model.question_w], feed_dict)
+    return loss, wi, ws, wq
 
 
 def eval_step(testset: dict, train_r, test_r, batch_size : int):
@@ -335,19 +416,21 @@ def eval_step(testset: dict, train_r, test_r, batch_size : int):
     # TODO: write to file
 
 
-def get_train_instances(train_r_set: dict, train_s_set: dict):
-    user_train, item_train, social_train = [], [], []
+def get_train_instances(train_r_set: dict, train_s_set: dict, train_q_set: dict):
+    user_train, item_train, social_train, question_train = [], [], [], []
     for user in train_r_set.keys():
         user_train.append(user)
         item_train.append(train_r_set[user])
         social_train.append(train_s_set[user])
+        question_train.append(train_q_set[user])
 
     user_train = np.array(user_train)
     item_train = np.array(item_train)
     social_train = np.array(social_train)
+    question_train = np.array(question_train)
     user_train = user_train[:, np.newaxis]
 
-    return user_train, item_train, social_train
+    return user_train, item_train, social_train, question_train
 
 
 if __name__ == '__main__':
@@ -359,6 +442,7 @@ if __name__ == '__main__':
     i_test = np.array(test_data['sid'], dtype=np.int32)
     u_friend = np.array(social_data['uid'], dtype=np.int32)
     v_friend = np.array(social_data['sid'], dtype=np.int32)
+    # TODO: add questionnaire data
 
     n_train_users = np.ones(len(u_train))
     train_r = scipy.sparse.csr_matrix((n_train_users, (u_train, i_train)), dtype=np.int16, shape=(n_users, n_items))
@@ -389,7 +473,7 @@ if __name__ == '__main__':
         while len(train_set[i]) < max_item_pu:
             train_set[i].append(n_items)
 
-    # Building training for social domain
+    # Building training set for social domain
     train_f_set = {}
     max_social_pu = 0
     for i in range(len(u_friend)):
@@ -400,12 +484,13 @@ if __name__ == '__main__':
     for i in train_f_set.keys():
         if len(train_f_set[i]) > max_social_pu:
             max_social_pu = len(train_f_set[i])
-
     for i in train_set.keys():
         if not i in train_f_set:
             train_f_set[i] = [n_users]
         while len(train_f_set[i]) < max_social_pu:
             train_f_set[i].append(n_users)
+
+    # TODO: build training set for questionnaire domain like above
 
     batch_size = 128
     with tf.Graph().as_default():
@@ -423,7 +508,7 @@ if __name__ == '__main__':
 
             sess.run(tf.global_variables_initializer())
 
-            user_train, item_train, friend_train = get_train_instances(train_set, train_f_set)
+            user_train, item_train, friend_train, question_train = get_train_instances(train_set, train_f_set, train_q_set)
 
             for epoch in range(100):
                 print(f'Epoch: {epoch}')
@@ -433,6 +518,7 @@ if __name__ == '__main__':
                 user_train = user_train[shuffled_indices]
                 item_train = item_train[shuffled_indices]
                 friend_train = friend_train[shuffled_indices]
+                question_train = question_train[shuffled_indices]
 
                 n_batches = int(len(user_train) / batch_size)
                 loss = 0.0
@@ -443,8 +529,9 @@ if __name__ == '__main__':
                     u_batch = user_train[start_index:end_index]
                     i_batch = item_train[start_index:end_index]
                     f_batch = friend_train[start_index:end_index]
+                    q_batch = question_train[start_index:end_index]
 
-                    batch_loss, wi, wf = train_step(u_batch, i_batch, f_batch)
+                    batch_loss, wi, wf, wq = train_step(u_batch, i_batch, f_batch, q_batch)
                     loss += batch_loss
                 print('\r\tUpdating: time=%.2f'
                       % (time.time() - start_t))
