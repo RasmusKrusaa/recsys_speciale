a04ccba4ad639e33d9d8ebc17130b6a3699b46b8
RasmusKrusaa <rasmuskrusaa@gmail.com>
2020-05-25 08:43:34 +0200

    data

--- data/ciao_explicit_preprocessed/tmp.py
+++ data/ciao_explicit_preprocessed/tmp.py
@@ -1,21 +1,19 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    ratings = pd.read_csv('../ciaoDVD/movie-ratings.txt', sep=',', usecols=[0, 1, 4], header=None)
-    ratings.columns = ['uid', 'iid', 'rating']
+    ratings = pd.read_csv('../../EATNN/data/ciao_from_them/ratings.csv', sep=',')
     ratings = ratings.sort_values(by=['uid', 'iid'])
 
     # removing items with fewer than 5 interactions
     ratings = ratings.groupby('iid').filter(lambda iid: len(iid) >= 5)
 
-    users = list(ratings['uid'])
-    items = list(ratings['iid'])
+    users = list(ratings['uid'].unique())
+    items = list(ratings['iid'].unique())
 
-    with open('raw_ratings.csv', 'w', newline='') as f:
+    with open('../../EATNN/data/ciao_from_them/raw_ratings.csv', 'w', newline='') as f:
         ratings.to_csv(f, index=False)
 
-    social_data = pd.read_csv('../ciaoDVD/trusts.txt', sep=',', usecols=[0, 1], header=None)
-    social_data.columns = ['uid', 'sid']
+    social_data = pd.read_csv('../../EATNN/data/ciao_from_them/trust.csv', sep=',')
     social_data = social_data.sort_values(by=['uid', 'sid'])
 
     for row in social_data.itertuples():
@@ -26,5 +24,5 @@ if __name__ == '__main__':
             print(f'removing row with friendsship between: {uid} and {sid}')
             social_data.drop(social_data[(social_data['uid'] == uid) & (social_data['sid'] == sid)].index, inplace=True)
 
-    with open('raw_trust_with_removed_friendsships.csv', 'w', newline='') as f:
+    with open('../../EATNN/data/ciao_from_them/raw_trust.csv', 'w', newline='') as f:
         social_data.to_csv(f, index=False)
